88c60ce08cf94913a18cdc7fc33833d963c05e35
Daniel Longacre <danieljlongacre@gmail.com>
2023-08-13 19:51:16 -0400

    Reformatted API calls so there is only 1 function, added clear button to reload page between searches, and added comments

--- Assets/main.js
+++ Assets/main.js
@@ -1,10 +1,15 @@
+//Global Variables
 var songID = '';
 var height = '';
 var width = '';
 var pic = '';
-// var track_id;
+var trackID = '';
+var lyrics = '';
 var iframe = document.getElementById('player');
+var trackLyricsURL = '';
 
+
+//Click function
 function btnClick() {
     $("#inputGroup-sizing-lg").click(function(){
         var userSong = $("#userSong").val();
@@ -16,78 +21,91 @@ function btnClick() {
 }
 
 
+//Reload the page after every search
+document.getElementById("clear").addEventListener("click", function() {
+    location.reload();
+});
+
+
 //YOUTUBE API SECTION
+//DON'T DELETE THIS!!!!!!
+//This is commented out so that we don't go over
+//  the alloted amount of API requests.
+// There is a daily limit.
 
 function APIcall() {
-    var userSong = $("#userSong").val() + " cover";
-    var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet%2C%20id&maxResults=1&q=';
-    var APIKey = '&videoEmbeddable=any&key=AIzaSyC-AUJX5gMJ-aKoHP0yZz3Sl0Q0-k6-92o';
-    var queryURL = url + userSong + APIKey;
-    // var newURL = 'https://www.youtube.com/watch?v=';
+    var userSong = $("#userSong").val() + ' cover';
+    // var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet%2C%20id&maxResults=1&q=';
+    // var APIKey = '&videoEmbeddable=any&key=AIzaSyC-AUJX5gMJ-aKoHP0yZz3Sl0Q0-k6-92o';
+    // var queryURL = url + userSong + APIKey;
      
-    fetch(queryURL).then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data) {
-                songID = data.items[0].id.videoId;
-                height = data.items[0].snippet.thumbnails.high.height;
-                width = data.items[0].snippet.thumbnails.high.width;
-                pic = data.items[0].snippet.thumbnails.high.url;
-                console.log(songID);
-                console.log(height);
-                console.log(width);
-                $('#player').attr('src', 'https://www.youtube.com/embed/' + songID + '?autoplay=1?enablejsapi=1');
-                // $('#player').attr('src', pic);
-                $('#player').attr('width', width);
-                // $('#player').attr('height', height);
-                // newURL += songID;
-            })
-        }
-    })
+    // fetch(queryURL).then(function(response) {
+    //     if(response.ok) {
+    //         response.json().then(function(data) {
+    //             songID = data.items[0].id.videoId;
+    //             height = data.items[0].snippet.thumbnails.high.height;
+    //             width = data.items[0].snippet.thumbnails.high.width;
+    //             console.log(songID);
+    //             console.log(height);
+    //             console.log(width);
+    //             $('#player').attr('src', 'https://www.youtube.com/embed/' + songID + '?autoplay=1?enablejsapi=1');
+    //             $('#player').attr('width', width);
+    //             $('#player').attr('height', height);
+    //         })
+    //     }
+    // })
     
 }
 
 
-btnClick();
+//API call to first get track ID and then, using that ID, 
+// make another call to display the lyrics
+async function trackIDAPIcall() {
+    var userSong = $("#userSong").val();
+    var trackIDURL = 'https://spotify23.p.rapidapi.com/search/?q=' + userSong + '&type=tracks&offset=0&limit=10&numberOfTopResults=5';
 
-
-// MUSICXMATCH API SECTION
-
-function trackIDAPIcall() {
-    var userInput = $("#userSong").val();
-    var url = 'http://api.musixmatch.com/ws/1.1/track.search?page_size=1&page=1&s_track_rating=desc';
-    var APIKey = '&apikey=c31a3caa2fcc7a9c42c1363b7ce5ca85&q_track=';
-    var trackURL = url + APIKey +userInput; 
-
-    fetch(trackURL).then(function(response) {
+    fetch(trackIDURL, {method: 'GET', headers: {
+        'X-RapidAPI-Key': '8a4e712e37mshb1ed90c6fc2827fp13454djsnb60e061840ac',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'}
+    }).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
-                track_id = data.message.body.track_list[0].track.track_id;
-                $("#trackID").text() = track_id;
-                console.log(track_id);
+                trackID = data.tracks.items[0].data.id;
+                // $('#trackID').text(trackID);
+                trackLyricsURL = 'https://spotify23.p.rapidapi.com/track_lyrics/?id=' + trackID;
+
+                fetch(trackLyricsURL, {method: 'GET', headers: {
+                    'X-RapidAPI-Key': '8a4e712e37mshb1ed90c6fc2827fp13454djsnb60e061840ac',
+                    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+                }})
+                .then(response => response.json())
+                .then(data => {
+                    if (data && data.lyrics && Array.isArray(data.lyrics.lines)) {
+                        data.lyrics.lines.forEach(line => {
+                            displayLyrics(line.words);
+                        });
+                    } else {
+                        console.log('Lyrics data format is incorrect.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching lyrics:', error);
+                });
             })
-        } 
+        }
     })
 
 }
 
 
-
-// to do items
-// get search query from search bar
-
-// use it to make a request to the api server
-// function to call the server
-
-
-// looking to get track name, lyrics, and artist name
+//Creating container for lyrics
+var lyricsContainer = document.getElementById('lyrics')
+var displayLyrics = (lyrics) => {
+    lyricsContainer.innerHTML += `
+    <p>${lyrics}</p>
+    `
+}
 
 
-
-// to do items
-// get search query from search bar
-
-// use it to make a request to the api server
-// function to call the server
-
-
-// looking to get track name, lyrics, and artist name
+//Call the functions
+btnClick();
